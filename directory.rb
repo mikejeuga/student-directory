9d839729d0120e3b875ab4ffd06fb3db7cd68482
Mike Jeuga <mikejeuga@gmail.com>
2021-03-22 17:40:04 +0000

    Numbered listing of the students.

--- directory.rb
+++ directory.rb
@@ -1,25 +1,28 @@
 # let's put all the students into an array.
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vador",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates",
-]
 
+# students = [
+#   { name: "Dr. Hannibal Lecter", cohort: :november },
+#   { name: "Darth Vador", cohort: :november },
+#   { name: "Nurse Ratched", cohort: :november },
+#   { name: "Michael Corleone", cohort: :november },
+#   { name: "The Wicked Witch of the West", cohort: :november },
+#   { name: "Terminator", cohort: :november },
+#   { name: "Freddy Krueger", cohort: :november },
+#   { name: "The Joker", cohort: :november },
+#   { name: "Joffrey Baratheon", cohort: :november },
+#   { name: "Norman Bates", cohort: :november },
+
+# ]
 # and print them
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of my cohort at Makers"
   puts "-------------"
 end
 
 def print(names)
-  names.each { |name| puts name }
+  names.each { |name|
+    puts "#{names.index(name) + 1}. #{name[:name]} (#{name[:cohort]} cohort)"
+  }
 end
 
 # finally, we print the total number of students.
@@ -27,6 +30,24 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "PLease enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header()
 print(students)
 print_footer(students)
